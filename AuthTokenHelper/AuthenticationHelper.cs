bfc73f9c234147f7f7cf196d70b1b1e03e7f6415
Mukul Joshi <mjoshi@farahexperiences.com>
2020-06-03 15:41:27 +0530

    Add project files.

---
+++ AuthTokenHelper/AuthenticationHelper.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace AuthTokenHelper
+{
+    public class AuthenticationHelper
+    {
+        public static TokenResponse GetBasicAuthSignInToken(string tokenurl, string username, string password)
+        {
+            TokenResponse response = new TokenResponse();
+            string responseToken = GetBasicToken(tokenurl, username, password);
+            JObject jobj = JObject.Parse(responseToken);
+            string token = (string)jobj["access_token"];
+            if (token == null)
+            {
+                string error = (string)jobj["error"];
+                string description = (string)jobj["error_description"];
+                response.token = "error:" + error + " ,error description : " + description;
+                response.isSuccess = false;
+            }
+            else
+            {
+                response.token = token;
+                response.isSuccess = true;
+            }
+
+            return response;
+        }
+
+        static string GetBasicToken(string url, string username, string password)
+        {
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>( "grant_type", "client_credentials")
+            };
+
+            var content = new FormUrlEncodedContent(pairs);
+            var byteArray = Encoding.ASCII.GetBytes(username + ":" + password);
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var response = client.PostAsync(url, content).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+
+        }
+
+    }
+}
---
+++ AuthTokenHelper/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthTokenHelper
+{
+    public class TokenResponse
+    {
+        public string token { get; set; }
+        public bool isSuccess { get; set; }
+    }
+}
